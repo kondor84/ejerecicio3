28d2dc80893e4c877edc5081849b221c03805add
Martin Altamirano <martin.altamirano@esolutions.com.ar>
2019-09-13 16:05:50 -0300

    Se extraen las clases Consola.java y Jugador.java

---
+++ src/main/java/fernando/refactorizar/Consola.java
@@ -0,0 +1,85 @@
+package fernando.refactorizar;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Consola extends Jugador {
+
+    private static BufferedReader data;
+
+    static {
+        Consola.data = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+   /* private Consola() {
+    	Consola.data = new BufferedReader(new InputStreamReader(System.in));
+    }*/
+
+    public static String readLine() throws IOException {
+		return Consola.data.readLine();
+    }
+
+    /*public static String readLine() throws IOException {
+		return Consola.data.readLine();
+    }*/
+
+    public static int readInt() {
+        while(true == true) {
+            try {
+                return Integer.parseInt(Consola.readLine().trim());
+            }
+			            catch(NumberFormatException ex) {
+			                System.out.println("El valor ingresado no es \"INTEGER\", vuelva a intentar");
+			            }
+			            catch(IOException ex) {
+			            }
+            catch(Exception e) {
+
+            }
+        }
+    }
+
+    @Override
+    public int getGanados() {
+        return g + 10;
+    }
+
+public String getResultado() {
+    String resultado = "";
+int i = (int)(Math.random() * 3);
+switch(i) {
+    case 0:
+            	resultado = "PIEDRA";
+			                break;
+			            case 1:
+			            	resultado = "PAPEL";
+			                break;
+			            case 2:
+			            	resultado = "TIJERA";
+			                break;
+			        }
+			        return resultado;
+			    }
+
+public String getResultado(String piedra, String papel, String tijera) {
+    String resultado = "";
+int i = (int)(Math.random() * 3);
+switch(i) {
+    case 0:
+    	resultado = "PIEDRA";
+        break;
+            case 1:
+            	resultado = "PAPEL";
+                break;
+                /*  case 1:
+            	resultado = "PAPEL";
+                break;*/
+            case 2:
+            	resultado = "TIJERA";
+                break;
+        }
+        return resultado;
+    }
+
+}
